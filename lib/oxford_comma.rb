982415a2124e20deb6f6740bb402f47603270c5a
Victoria Meng <nights1ststar@protonmail.com>
2018-03-04 01:52:30 +0000

    automatically backed up by learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -3,8 +3,15 @@ items = %w(apple bat cat)
 items.join(", ")
 
 def oxford_comma(items)
-  items[-1] = "and #{items[-1]}"
-  items.join(", ")
+  if items.size == 1
+    items.join(", ")
+  elsif items.size == 2
+    items[-1] = "and #{items[-1]}"
+    items.join(" ")
+  else
+    items[-1] = "and #{items[-1]}"
+    items.join(", ")
+  end
 end
 
 oxford_comma(items)
