f1ca5e4899d2e9a45f71b1705223ddd34b0ce5e7
Victoria Meng <nights1ststar@protonmail.com>
2018-03-04 01:54:23 +0000

    automatically backed up by learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -3,13 +3,10 @@ items = %w(apple bat cat)
 items.join(", ")
 
 def oxford_comma(items)
-  if items.size == 1
-    items.join(", ")
-  elsif items.size == 2
-    items[-1] = "and #{items[-1]}"
+  items[-1] = "and #{items[-1]}" if items.size >= 2  
+  if items.size == 2  
     items.join(" ")
   else
-    items[-1] = "and #{items[-1]}"
     items.join(", ")
   end
 end
